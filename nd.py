6559aab42b4d0949bb5084d01ba85536fff9ac3d
MrJonas <jonas.l.antanaitis@gmail.com>
2014-03-14 14:20:39 +0200

    Script allows user to enter directory path. Also checks if directory path is entered correctly and exist.

---
+++ nd.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+#! coding: utf:-8
+import sys
+import os
+# Checks if entered directory path exist:
+if len(sys.argv) < 2:
+    print("Enter directory path")
+    quit()
+elif not os.path.isdir(sys.argv[1]):
+    print("Entered directory does not exists")
+    quit()
+else:
+    directory_path = sys.argv[1]
