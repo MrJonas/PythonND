b12f4d6b6528dd20d13545edd5faf3e26e62320d
MrJonas <jonas.l.antanaitis@gmail.com>
2014-03-15 12:48:24 +0200

    Writes words and symbols statistics from each file to Directory_Stats.txt.

--- nd.py
+++ nd.py
@@ -42,7 +42,7 @@ def count_words(text):
 
 # Funtion to to write sorted dict to stats file:
 def dict_print(file_dict, sum_dict):
-    for key in sorted(file_dict, key = file_dict.get, reverse = True):
+    for key in sorted(file_dict, key=file_dict.get, reverse=True):
         if str(key) == " ":
             stats_file.write(str(file_dict[key]) + ' Space\n')
         elif str(key) == "\n":
@@ -55,11 +55,22 @@ def dict_print(file_dict, sum_dict):
 stats_file_path = str(directory_path) + str('Directory_Statistics.txt')
 stats_file = open(stats_file_path, 'w+')
 # Reads all files in specified directory:
+symbols = {}
+words = {}
+sum_symbols = {}
+sum_words = {}
 for data in os.listdir(directory_path):
     file_path = str(directory_path) + str(data)
     if os.path.isfile(file_path) and file_path != stats_file_path:
         read_file = open(file_path, 'r')
         text = read_file.read()
         read_file.close()
+        symbols = count_symbols(text)
+        words = count_words(text)
+        stats_file.write('File:\n' + str(file_path) + '\nWords statistics:\n')
+        sum_words = dict_print(words, sum_words)
+        stats_file.write('File:\n' + str(file_path))
+        stats_file.write('\nSymbols statistics:\n')
+        sum_symbols = dict_print(symbols, sum_symbols)
 # Closes stats file:
 stats_file.close()
