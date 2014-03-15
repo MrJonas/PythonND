0246b37134d76ce2033b89a5c770fb0a74cf8c02
MrJonas <jonas.l.antanaitis@gmail.com>
2014-03-15 11:33:11 +0200

    Stats file created in the user defined directory. Also for loop created to read all files in the same directory.

--- nd.py
+++ nd.py
@@ -38,3 +38,16 @@ def count_words(text):
         else:
             new_word = str(new_word) + str(text[i])
     return words
+# Creates stats file in specified directory:
+stats_file_path = str(directory_path) + str('Directory_Statistics.txt')
+stats_file = open(stats_file_path, 'w+')
+# Reads all files in specified directory:
+for data in os.listdir(directory_path):
+    file_path = str(directory_path) + str(data)
+    if os.path.isfile(file_path):
+        read_file = open(file_path, 'r')
+        text = read_file.read()
+        read_file.close()
+        print(text)
+# Closes stats file:
+stats_file.close()
